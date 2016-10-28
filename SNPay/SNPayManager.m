70bc32a2fd6de98fea49ec4e1a481016d1212233
w_sen <wangsenwd@hotmail.com>
2016-10-28 09:23:37 +0800

    替换新sdk

--- SNPay/SNPayManager.m
+++ SNPay/SNPayManager.m
@@ -76,68 +76,100 @@ static SNPayManager * _manager = nil;
 
 
 #pragma mark - ip
-//获取ip地址
-- (NSString *)getIPAddress:(BOOL)preferIPv4 {
-    
-    NSArray *searchArray = preferIPv4 ?
-    @[ IOS_VPN @"/" IP_ADDR_IPv4, IOS_VPN @"/" IP_ADDR_IPv6, IOS_WIFI @"/" IP_ADDR_IPv4, IOS_WIFI @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6 ] :
-    @[ IOS_VPN @"/" IP_ADDR_IPv6, IOS_VPN @"/" IP_ADDR_IPv4, IOS_WIFI @"/" IP_ADDR_IPv6, IOS_WIFI @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4 ] ;
-    
-    NSDictionary *addresses = [self getIPAddresses];
-    NSLog(@"addresses: %@", addresses);
-    
-    __block NSString *address;
-    [searchArray enumerateObjectsUsingBlock:^(NSString *key, NSUInteger idx, BOOL *stop)
-     {
-         address = addresses[key];
-         if(address) *stop = YES;
-     } ];
-    return address ? address : @"0.0.0.0";
-}
+////获取ip地址
+//- (NSString *)getIPAddress:(BOOL)preferIPv4 {
+//    
+//    NSArray *searchArray = preferIPv4 ?
+//    @[ IOS_VPN @"/" IP_ADDR_IPv4, IOS_VPN @"/" IP_ADDR_IPv6, IOS_WIFI @"/" IP_ADDR_IPv4, IOS_WIFI @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6 ] :
+//    @[ IOS_VPN @"/" IP_ADDR_IPv6, IOS_VPN @"/" IP_ADDR_IPv4, IOS_WIFI @"/" IP_ADDR_IPv6, IOS_WIFI @"/" IP_ADDR_IPv4, IOS_CELLULAR @"/" IP_ADDR_IPv6, IOS_CELLULAR @"/" IP_ADDR_IPv4 ] ;
+//    
+//    NSDictionary *addresses = [self getIPAddresses];
+//    NSLog(@"addresses: %@", addresses);
+//    
+//    __block NSString *address;
+//    [searchArray enumerateObjectsUsingBlock:^(NSString *key, NSUInteger idx, BOOL *stop)
+//     {
+//         address = addresses[key];
+//         if(address) *stop = YES;
+//     } ];
+//    return address ? address : @"0.0.0.0";
+//}
+//
+//- (NSDictionary *)getIPAddresses {
+//    
+//    NSMutableDictionary *addresses = [NSMutableDictionary dictionaryWithCapacity:8];
+//    
+//    // retrieve the current interfaces - returns 0 on success
+//    struct ifaddrs *interfaces;
+//    if(!getifaddrs(&interfaces)) {
+//        // Loop through linked list of interfaces
+//        struct ifaddrs *interface;
+//        for(interface=interfaces; interface; interface=interface->ifa_next) {
+//            if(!(interface->ifa_flags & IFF_UP) /* || (interface->ifa_flags & IFF_LOOPBACK) */ ) {
+//                continue; // deeply nested code harder to read
+//            }
+//            const struct sockaddr_in *addr = (const struct sockaddr_in*)interface->ifa_addr;
+//            char addrBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
+//            if(addr && (addr->sin_family==AF_INET || addr->sin_family==AF_INET6)) {
+//                NSString *name = [NSString stringWithUTF8String:interface->ifa_name];
+//                NSString *type;
+//                if(addr->sin_family == AF_INET) {
+//                    if(inet_ntop(AF_INET, &addr->sin_addr, addrBuf, INET_ADDRSTRLEN)) {
+//                        type = IP_ADDR_IPv4;
+//                    }
+//                } else {
+//                    const struct sockaddr_in6 *addr6 = (const struct sockaddr_in6*)interface->ifa_addr;
+//                    if(inet_ntop(AF_INET6, &addr6->sin6_addr, addrBuf, INET6_ADDRSTRLEN)) {
+//                        type = IP_ADDR_IPv6;
+//                    }
+//                }
+//                if(type) {
+//                    NSString *key = [NSString stringWithFormat:@"%@/%@", name, type];
+//                    addresses[key] = [NSString stringWithUTF8String:addrBuf];
+//                }
+//            }
+//        }
+//        // Free memory
+//        freeifaddrs(interfaces);
+//    }
+//    return [addresses count] ? addresses : nil;
+//}
+//
+//- (NSString *)spbill_create_ip {
+//    NSString * string;
+//    string =  [self getIPAddress:YES];
+//    return string;
+//}
 
-- (NSDictionary *)getIPAddresses {
-    
-    NSMutableDictionary *addresses = [NSMutableDictionary dictionaryWithCapacity:8];
-    
+- (NSString *)fetchIPAddress {
+    NSString *address = @"0.0.0.0";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
     // retrieve the current interfaces - returns 0 on success
-    struct ifaddrs *interfaces;
-    if(!getifaddrs(&interfaces)) {
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
         // Loop through linked list of interfaces
-        struct ifaddrs *interface;
-        for(interface=interfaces; interface; interface=interface->ifa_next) {
-            if(!(interface->ifa_flags & IFF_UP) /* || (interface->ifa_flags & IFF_LOOPBACK) */ ) {
-                continue; // deeply nested code harder to read
-            }
-            const struct sockaddr_in *addr = (const struct sockaddr_in*)interface->ifa_addr;
-            char addrBuf[ MAX(INET_ADDRSTRLEN, INET6_ADDRSTRLEN) ];
-            if(addr && (addr->sin_family==AF_INET || addr->sin_family==AF_INET6)) {
-                NSString *name = [NSString stringWithUTF8String:interface->ifa_name];
-                NSString *type;
-                if(addr->sin_family == AF_INET) {
-                    if(inet_ntop(AF_INET, &addr->sin_addr, addrBuf, INET_ADDRSTRLEN)) {
-                        type = IP_ADDR_IPv4;
-                    }
-                } else {
-                    const struct sockaddr_in6 *addr6 = (const struct sockaddr_in6*)interface->ifa_addr;
-                    if(inet_ntop(AF_INET6, &addr6->sin6_addr, addrBuf, INET6_ADDRSTRLEN)) {
-                        type = IP_ADDR_IPv6;
-                    }
-                }
-                if(type) {
-                    NSString *key = [NSString stringWithFormat:@"%@/%@", name, type];
-                    addresses[key] = [NSString stringWithUTF8String:addrBuf];
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
                 }
             }
+            temp_addr = temp_addr->ifa_next;
         }
-        // Free memory
-        freeifaddrs(interfaces);
     }
-    return [addresses count] ? addresses : nil;
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
 }
 
 - (NSString *)spbill_create_ip {
     NSString * string;
-    string =  [self getIPAddress:YES];
+    string =  [self fetchIPAddress];
     return string;
 }
 
